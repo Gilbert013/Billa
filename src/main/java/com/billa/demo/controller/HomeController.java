2056b5ec1fb3a9108fd3fba9eabb7734478e0216
totojang18 <totojang18@naver.com>
2025-10-19 23:05:46 +0900

    init some html

---
+++ src/main/java/com/billa/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.billa.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+
+        return "home";
+    }
+
+}
